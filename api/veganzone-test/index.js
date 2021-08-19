ec87e846c778d0dbda35fdbb80e236e4d9dd84be
Aykut Demirel <aykut.demirel@oredata.com>
2021-08-20 02:27:22 +0300

    [Improvement] rest api method changed

--- api/veganzone-test/index.js
+++ api/veganzone-test/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var controller = require('./test.controller');
 var router = express.Router();
 
-router.post('/start', controller.start);
-router.post('/stop', controller.stop);
+router.get('/start', controller.start);
+router.get('/stop', controller.stop);
 
 module.exports = router;
